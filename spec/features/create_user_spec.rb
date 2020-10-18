d673d801a4a342a2c8349c09c69f152daeb02786
Frank <frank@jaamo.nl>
2020-10-18 15:08:14 +0200

    wrote second test

--- spec/features/create_user_spec.rb
+++ spec/features/create_user_spec.rb
@@ -1,12 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating an author', type: :feature do
+	before {visit new_user_registration_path}
+
 	scenario 'valid inputs' do
-		visit new_user_registration_path
 		fill_in 'Email', with: 'Carl@Sagan.com'
 		fill_in 'Password', with: 'test12345'
 		fill_in 'Password confirmation', with: 'test12345'
 		click_on 'Sign up'
 		expect(page).to have_content('Welcome! You have signed up successfully.')
+	end
+
+	scenario 'invalid inputs' do
+		fill_in 'Email', with: ''
+		fill_in 'Password', with: 'test12345'
+		fill_in 'Password confirmation', with: 'test12345'
+		click_on 'Sign up'
+		expect(page).to have_content("Email can't be blank")
 	end
 end
