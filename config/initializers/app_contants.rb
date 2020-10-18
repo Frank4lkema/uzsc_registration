f971885778b88a17b8c7906ad6c16068978b6936
Frank <frank@jaamo.nl>
2020-10-18 20:18:51 +0200

    added team to the user

---
+++ config/initializers/app_contants.rb
@@ -0,0 +1,1 @@
+TEAMS = ["Heren 1","Heren 2","Heren 3","Heren 4","Heren 5","Heren 6","Heren 7","Heren 8","Heren 9","Dames 1","Dames 2","Dames 3","Dames 4","Dames 5", ]
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   end
 
   get 'dashboard/home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
