640b50e8d5a2a9e30ca72e192ec19aa19947886e
Frank <frank@jaamo.nl>
2020-10-18 18:19:32 +0200

    fixed the css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+	def pretty_date(date)
+		if date
+			date.strftime('%d-%m-%Y')
+		end
+	end
+
+	def pretty_time(date_time)
+		if date_time
+			date_time.strftime('%H:%M')
+		end
+	end
 end
