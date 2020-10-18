52d0d02f98289494fa702451bb38da627a1205bc
Frank <frank@jaamo.nl>
2020-10-18 13:34:01 +0200

    fixed the devise set up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  root to: "dashboard#home"
+  devise_for :users
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
 
   get 'dashboard/home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
