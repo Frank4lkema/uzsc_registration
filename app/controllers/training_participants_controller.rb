88635c30e62842c253f68578a902b955a90d49ad
Frank <frank@jaamo.nl>
2020-10-22 16:07:37 +0200

    made the routes

---
+++ app/controllers/training_participants_controller.rb
@@ -0,0 +1,3 @@
+class TrainingParticipantsController < ApplicationController
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  devise_scope :user do
-    root to: "devise/sessions#new"
-  end
+	devise_for :users
+	devise_scope :user do
+		root to: "devise/sessions#new"
+	end
 
-  get 'dashboard/home'
+	get 'dashboard/home'
+	resources :training_participants
 
 end
