183c64c190925ef057397c2892c6436e49b0cdd5
Frank <frank@jaamo.nl>
2020-10-22 16:09:03 +0200

    made the cotnrolelr action

--- app/controllers/training_participants_controller.rb
+++ app/controllers/training_participants_controller.rb
@@ -1,3 +1,13 @@
 class TrainingParticipantsController < ApplicationController
-
+	def create
+		@training = Training.find(params[:training_id])
+		if @training.training_participants.count < @training.max_participants
+			TrainingParticipant.create(user_id:current_user.id, training_id:@training.id)
+			flash[:notice] = "Aangemeld voor training"
+			redirect_to root_path
+		else
+			flash[:alert] = "Maximaal aantal personen bereikt"
+			redirect_to root_path
+		end
+	end
 end
