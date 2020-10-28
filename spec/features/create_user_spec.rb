ac927c8996f1720fed888bf97e599d40672ae8b4
Frank <frank@jaamo.nl>
2020-10-28 11:59:55 +0100

    start working on the login page

--- spec/features/create_user_spec.rb
+++ spec/features/create_user_spec.rb
@@ -1,21 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating an author', type: :feature do
-	before {visit new_user_registration_path}
+	before {visit new_user_session_path}
 
-	scenario 'valid inputs' do
-		fill_in 'Email', with: 'Carl@Sagan.com'
-		fill_in 'Password', with: 'test12345'
-		fill_in 'Password confirmation', with: 'test12345'
-		click_on 'Sign up'
+	scenario 'Inloggen juist' do
+		fill_in 'E-mail', with: 'Carl@Sagan.com'
+		fill_in 'Wachtwoord', with: 'test12345'
+		click_on 'Inloggen'
 		expect(page).to have_content('Welcome! You have signed up successfully.')
 	end
 
-	scenario 'invalid inputs' do
-		fill_in 'Email', with: ''
-		fill_in 'Password', with: 'test12345'
-		fill_in 'Password confirmation', with: 'test12345'
-		click_on 'Sign up'
-		expect(page).to have_content("Email can't be blank")
+	scenario 'Inloggen geen e-mail' do
+		fill_in 'E-mail', with: ''
+		fill_in 'Wachtwoord', with: 'test12345'
+		click_on 'Inloggen'
+		expect(page).to have_content("Invalid Email or password")
 	end
 end
