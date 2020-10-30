902f14279610fc5d69187478ac8020f65a999ffc
Frank <frank@jaamo.nl>
2020-10-30 12:11:09 +0100

    changes the dashboard to something nice now

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,13 @@ module ApplicationHelper
 			date_time.strftime('%H:%M')
 		end
 	end
+
+
+	def day_date_month(date)
+		date_array =[]
+		date_array << All_WEEK_DAYS[((date.cwday) -1)]
+		date_array << date.day.to_s
+		date_array << All_MONTHS[((date.month) -1)]
+		date_array.join(" ")
+	end
 end
--- config/initializers/app_contants.rb
+++ config/initializers/app_contants.rb
@@ -1,3 +1,5 @@
 TEAMS = ["Heren 1","Heren 2","Heren 3","Heren 4","Heren 5","Heren 6","Heren 7","Heren 8","Heren 9","Dames 1","Dames 2","Dames 3","Dames 4","Dames 5","Jeugd" ]
 USER_ROLES = [:admin, :player]
 BASIC_PASSWORD = "Welkom12345"
+All_WEEK_DAYS = ["Maandag", "Dinsdag","Woensdag","Donderdag","Vrijdag","Zaterdag","Zondag"]
+All_MONTHS = ["Januari", "Februari", "Maart", "April","Mei","Juni", "Juli", "Augustus","September","Okotber","November","December"]
